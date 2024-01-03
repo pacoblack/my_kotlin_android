eab622a0f037418f1d8149139f5ac479f0845637
paco <pacoson.wang@gmail.com>
2024-01-03 15:43:55 +0800

    添加

---
+++ app/src/main/java/com/example/myapplication/ExoPlayerDownloader.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+import com.google.android.exoplayer2.upstream.DataSpec
+import com.google.android.exoplayer2.upstream.cache.CacheKeyFactory
+
+class ExoPlayerDownloader {
+
+}
