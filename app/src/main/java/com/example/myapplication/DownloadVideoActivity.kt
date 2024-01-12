c9d0cfe312d0b944a11253178b05e14b02811779
paco <pacoson.wang@gmail.com>
2024-01-12 17:27:13 +0800

    add play button

--- app/src/main/java/com/example/myapplication/DownloadVideoActivity.kt
+++ app/src/main/java/com/example/myapplication/DownloadVideoActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -11,12 +12,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.gang.video.service.DemoDownloadService
 import com.google.android.exoplayer2.offline.DownloadManager
 import com.google.android.exoplayer2.offline.DownloadRequest
-import com.google.android.exoplayer2.upstream.DataSource
+import com.test.gang.lib.video.IPlayerActivity
 
 
 class DownloadVideoActivity : AppCompatActivity() {
     private lateinit var downloadUrlInput: EditText
     private lateinit var startDownloadButton: Button
+    private lateinit var startPlayButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_download)
@@ -24,6 +26,7 @@ class DownloadVideoActivity : AppCompatActivity() {
         // 初始化控件
         downloadUrlInput = findViewById(R.id.download_url_input)
         startDownloadButton = findViewById(R.id.start_download_button)
+        startPlayButton = findViewById(R.id.start_play_button)
 
         // 假设你已经有了一个自定义的downloadFileFromUrl()方法用于下载文件
         startDownloadButton.setOnClickListener {
@@ -36,6 +39,21 @@ class DownloadVideoActivity : AppCompatActivity() {
                 Toast.makeText(this@DownloadVideoActivity, "请输入有效的下载地址", Toast.LENGTH_SHORT).show()
             }
         }
+
+        startPlayButton.setOnClickListener {
+            val url = downloadUrlInput.text.toString().trim { it <= ' ' }
+            if (url.isNotEmpty()) {
+                // 这里仅作为示例，你需要替换为实际的下载逻辑
+                playVideoFromUrl(this@DownloadVideoActivity, url)
+            } else {
+                // 输入框为空时提示用户
+                Toast.makeText(this@DownloadVideoActivity, "请输入有效的下载地址", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun playVideoFromUrl(activity: Activity, url: String) {
+        IPlayerActivity.Companion.startActivity(activity, url);
     }
 
     // 实际的下载文件方法，这里仅作占位，具体实现请参考ExoPlayer或其他库的下载功能
--- app/src/main/java/com/example/myapplication/ExoPlayerDownloader.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication
-
-import com.google.android.exoplayer2.upstream.DataSpec
-import com.google.android.exoplayer2.upstream.cache.CacheKeyFactory
-
-class ExoPlayerDownloader {
-
-}
