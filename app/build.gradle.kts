b8d44bf493cb3e1e58fbe4312702319bbef1f36e
paco <pacoson.wang@gmail.com>
2026-04-25 11:11:51 +0800

    引入一个新的模块

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":third"))
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ third/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.find.gang.third"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        }
+    }
+    composeOptions {
+        // 关键：指定 Compose 编译器扩展版本，必须与 Compose BOM 版本匹配
+        kotlinCompilerExtensionVersion = "1.5.11"   // 以 Compose BOM 2024.11.00 为例
+    }
+    buildFeatures {
+        compose = true
+        viewBinding = true
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
